0bc45dd2f370148f8025675f14bbec42a99cebac
Landon Reasoner <reasoner.landon90@gmail.com>
2021-03-27 21:14:09 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.integer "price"
     t.integer "size"
-    t.string "image_url"
+    t.string "imageurl"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
